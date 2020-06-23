01a9f74785837de05a3750be771930f7def69776
Aidan Middleton <aidanemiddleton@gmail.com>
2020-06-23 15:23:18 -0700

    first commit

---
+++ Swiftui slot machine/Swiftui slot machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftui slot machine
+//
+//  Created by Aidan Middleton on 2020-06-23.
+//  Copyright © 2020 Aidan Middleton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
